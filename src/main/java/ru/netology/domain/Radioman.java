f41c640f59784e438fb9c4e879d56dbc309e37a8
Maksik <mudarcev@list.ru>
2023-02-01 12:11:13 +0700

    .gitignore

---
+++ src/main/java/ru/netology/domain/Radioman.java
@@ -0,0 +1,62 @@
+package ru.netology.domain;
+
+public class Radioman {
+    private int currentStation;
+    private final int minStation = 0;
+    private final int maxStation = 9;
+    private int currentVolume;
+    public final int maxVolume = 10;
+
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation || currentStation > maxStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (maxStation <= currentStation) {
+            setCurrentStation(minStation);
+        } else {
+            setCurrentStation(currentStation + 1);
+        }
+    }
+
+    public void previousStation() {
+        if (currentStation <= minStation) {
+            setCurrentStation(maxStation);
+        } else {
+            setCurrentStation(currentStation - 1);
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        int minVolume = 0;
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+
+}
